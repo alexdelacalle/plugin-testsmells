3806c1af5cdb0a5847a5096d081e38556ce3b281
alejandro albaladejo carrasco <116547430+alexdelacalle@users.noreply.github.com>
2025-12-17 22:04:39 +0100

    Delete src/main/java/es/upm/grise/profundizacion/OperacionDuplicadaException.java

--- src/main/java/es/upm/grise/profundizacion/OperacionDuplicadaException.java
+++
@@ -1,7 +0,0 @@
-package es.upm.grise.profundizacion;
-
-public class OperacionDuplicadaException extends RuntimeException {
-    public OperacionDuplicadaException(String mensaje) {
-        super(mensaje);
-    }
-}
