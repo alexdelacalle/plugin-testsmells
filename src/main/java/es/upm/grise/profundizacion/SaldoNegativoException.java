9eaf87c6b7c57ceebc5396b6f4a447106a2ff95a
alejandro albaladejo carrasco <116547430+alexdelacalle@users.noreply.github.com>
2025-12-17 22:04:29 +0100

    Delete src/main/java/es/upm/grise/profundizacion/SaldoNegativoException.java

--- src/main/java/es/upm/grise/profundizacion/SaldoNegativoException.java
+++
@@ -1,7 +0,0 @@
-package es.upm.grise.profundizacion;
-
-public class SaldoNegativoException extends RuntimeException {
-    public SaldoNegativoException(String mensaje) {
-        super(mensaje);
-    }
-}
